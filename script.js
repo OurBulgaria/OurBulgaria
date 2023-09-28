555c5d7470d49e4ba47fd98dc55c02d2daee2f10
OurBulgaria <142672888+OurBulgaria@users.noreply.github.com>
2023-09-29 00:11:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 let name = window.prompt("Как се казвате?");
 name1 = "Здравейте!<br>" + name + "<br>Добре дошли<br>в Нашата България!";
 document.getElementById("myLabel").innerHTML = name1;
+
+while(name == ""){
+    name = window.prompt("Как се казвате?");
+}с
